c4f72dd09732aed827615077aab1b51a20334f54
David G. Simmons <santafen@mac.com>
2022-03-17 09:45:58 -0400

    tons of shit

---
+++ electron-webpack-quick-start/src/main/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Translate } from './Translate';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+        <Translate />
+        </div>
+        </header>
+    </div>
+  );
+}
+
+export default App;
